c430d0d0efffb18644a42f6fd8f7aa029c844787
iman irandoost <imn.irdst@gmail.com>
2015-08-29 12:12:02 +0430

    264 th :: 840 ACs :: 1786 Subs

---
+++ UVa/Comptitive Programming/Ch-01 Ad Hoc/Game (others)/UVa 114 - Simulation Wizardry - AC.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int MAX = 52 + 10;
+
+int grid[MAX][MAX] = { 0 }, n, m, b, wall_cost;
+
+struct Bumper{
+	int v, c; Bumper(int v = 0, int c = 0):v(v), c(c){}
+}bmp[MAX*MAX];
+
+
+enum direction{ RIGHT, UP, LEFT, DOWN };
+
+const int nextDir[4] = {  DOWN, RIGHT, UP, LEFT };
+const int dx[]       = {    +1,     0, -1,    0 };
+const int dy[]       = {     0,    +1,  0,   -1 };
+
+int main(){
+	cin >> m >> n;
+	cin >> wall_cost >> b;
+
+	int x, y;
+	for (int i = 1; i <= b; i++){
+		cin >> x >> y >> bmp[i].v >> bmp[i].c;
+		grid[x][y] = i;
+	}
+
+	int ans = 0, dir, lifetime;
+	while (cin >> x >> y >> dir >> lifetime){
+		int val = 0;
+		while (--lifetime > 0){
+			int xx = x + dx[dir];
+			int yy = y + dy[dir];
+
+			if (xx == m || yy == m || yy == 1 || xx == 1){
+				lifetime -= wall_cost;
+				dir = nextDir[dir]; continue;
+			}
+
+			int id = grid[xx][yy];
+			if (id != 0){
+				lifetime -= bmp[id].c;
+				val      += bmp[id].v;
+				dir = nextDir[dir]; continue;
+			}
+
+			x = xx; y = yy;
+		}
+
+		cout << val << endl; ans += val;
+	}
+	cout << ans << endl;
+}
---
+++ UVa/Comptitive Programming/Ch-01 Ad Hoc/Palindrome and Anagram/UVa 11309 - Counting Chaos - AC.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+struct Time{
+	int hh, mm; string str;
+	Time(int hh = 0, int mm = 0)
+		:hh(hh), mm(mm){ updateString(); }
+	void inc(){
+		mm++;
+		if (mm == 60) mm = 0, hh++;
+		if (hh >= 24) hh = 0;
+		updateString();
+	}
+
+	bool isPalind(){
+		string tmp = str;
+		tmp.erase(2, 1); // remove ':'
+
+		string org = ""; bool flag=1;
+		for (int i = 0; i < tmp.length(); i++){
+			if (tmp[i] == '0' && flag) continue; 
+			else flag = 0, org += tmp[i];
+		}
+
+		string rev = org; 
+		reverse(rev.begin(), rev.end());
+		return rev == org;
+	}
+
+	void updateString(){
+		stringstream sstr; str.clear();
+		sstr << setw(2) << setfill('0') << hh;
+		sstr << ":";
+		sstr << setw(2) << setfill('0') << mm;
+		sstr >> str;
+	}
+};
+
+int main(){
+	int tc;
+	cin >> tc;
+	while (tc--){
+		int hh, mm; char c;
+		cin >> hh >> c >> mm;
+
+		Time tm(hh, mm); tm.inc();
+		while (!tm.isPalind()) 
+			tm.inc();
+
+		cout << tm.str << endl;
+	}
+
+}
